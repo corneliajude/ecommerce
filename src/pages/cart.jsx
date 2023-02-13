a7243dc3f0812cd8d7901c7b883489a25f28db52
corneliajude <judecornelia22@gmail.com>
2023-02-13 22:04:52 +0200

    ConsoleLogProducts

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,11 +1,16 @@
-import { CartControls, CartDisplay, CartTotals } from '@/components/cart';
+import {
+  cartContext,
+  CartControls,
+  CartDisplay,
+  CartTotals,
+} from '@/components/cart';
 import { ContinueShopping } from '@/components/ui/ContinueShopping';
 import { Layout } from '@/layouts';
 import Head from 'next/head';
+import { useContext } from 'react';
 
 const CartPage = () => {
-  // const { cart } = useContext(CartContext)
-  const { cart } = { cart: [] };
+  const { cartProducts, loading } = useContext(cartContext);
 
   return (
     <>
@@ -37,15 +42,19 @@ const CartPage = () => {
               <CartTotals></CartTotals>
 
               <div>
-                <button
-                  type="button"
-                  title="Proceed to checkout"
-                  onClick={() => {
-                    console.log(cart);
-                  }}
-                >
-                  Proceed to checkout
-                </button>
+                {!loading ? (
+                  <button
+                    type="button"
+                    title="Proceed to checkout"
+                    onClick={() => {
+                      console.log(cartProducts);
+                    }}
+                  >
+                    Proceed to checkout
+                  </button>
+                ) : (
+                  <></>
+                )}
               </div>
             </aside>
           </section>
